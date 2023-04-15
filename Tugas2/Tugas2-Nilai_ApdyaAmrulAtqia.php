3836c09afa43e1023b0e2af87bfc414b48090cce
Apdya06 <116083181+Apdya06@users.noreply.github.com>
2023-04-15 23:32:11 +0700

    Delete Tugas2 directory

--- Tugas2/Tugas2-Nilai_ApdyaAmrulAtqia.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mtk = rand(1,100); $fis = rand(1,100); $bio = rand(1,100);
-$rata = ($mtk + $fis + $bio) / 3;   
-    if ($rata >= 60)
-        echo 'Lulus Dengan Nilai: '. round($rata, 2);
-    else
-        echo 'Tidak Lulus Dengan Nilai: '. round($rata, 2);
-?>
