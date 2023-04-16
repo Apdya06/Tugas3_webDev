cd1342c5771b54854a8e7c69dd245f6f708e2e14
Apdya06 <116083181+Apdya06@users.noreply.github.com>
2023-04-16 17:53:49 +0700

    Update output.php

--- Tugas3/output.php
+++ Tugas3/output.php
@@ -13,62 +13,7 @@
 <html lang="en">
 <head>
     <title>My Biodata</title>
-    <style>
-        *{
-            border-radius: 5px;
-        }
-        body{
-            font-family: sans-serif;
-            background-color: black;
-        }
-        .container {
-            width: 50%;
-            padding: 70px 70px 70px 50px;
-            margin-right: auto;
-            margin-left: auto;
-            align-content: center;
-            justify-content: center;
-            background-color: rgb(247,247, 251);
-            border-radius: 5%;
-        }
-        .title{
-            font-size: 55px;
-            font-weight: 600;
-            margin: auto;
-        }
-        .subtitle{
-            font-size: 20px;
-            margin-top: 0;
-            margin-bottom: 25px;
-        }
-        .sz-20{
-            font-size: 20px;
-        }
-        .wdt100{
-            margin-top: 5px;
-            margin-bottom: 10px;
-            width: 100%;
-            box-sizing: content-box;
-            padding: 10px;
-            color: gray;
-            border: solid 1px gray;
-        }
-        .wdt100 :checked{
-            color: gray;
-
-        }
-        .sub{
-            width: 20%;
-            box-sizing: content-box;
-            padding: 10px 20px;
-            margin-top: 15px;
-            margin-left: 75%;
-            background-color: rgb(56, 161, 105);
-            color: white;
-            border: solid 1px gray;
-            font-size: 22px;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
     
