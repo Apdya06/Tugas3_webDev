20ad7fbd51d1296f998c9237e5fac76045ffe687
Apdya06 <116083181+Apdya06@users.noreply.github.com>
2023-04-16 17:52:16 +0700

    Update Input.php

--- Tugas3/Input.php
+++ Tugas3/Input.php
@@ -11,96 +11,10 @@
 <html lang="en">
 <head>
     <title>Form Biodata</title>
-    <style>
-        *{
-            border-radius: 5px;
-        }
-        body{
-            font-family: sans-serif;
-            background-color: black;
-        }
-        .container {
-            width: 50%;
-            padding: 70px 70px 70px 50px;
-            margin-right: auto;
-            margin-left: auto;
-            align-content: center;
-            justify-content: center;
-            background-color: rgb(247,247, 251);
-            border-radius: 5%;
-        }
-        .title{
-            font-size: 55px;
-            font-weight: 600;
-            margin: auto;
-        }
-        .subtitle{
-            font-size: 20px;
-            margin-top: 0;
-            margin-bottom: 25px;
-        }
-        .sz-20{
-            font-size: 20px;
-        }
-        .wdt100{
-            margin-top: 5px;
-            margin-bottom: 10px;
-            width: 100%;
-            box-sizing: content-box;
-            padding: 10px;
-            color: gray;
-            border: solid 1px gray;
-        }
-        .wdt100 :checked{
-            color: gray;
-
-        }
-        .sub{
-            width: 20%;
-            box-sizing: content-box;
-            padding: 10px 20px;
-            margin-top: 15px;
-            margin-left: 75%;
-            background-color: rgb(56, 161, 105);
-            color: white;
-            border: solid 1px gray;
-            font-size: 22px;
-        }
-
-        #modal{
-            position: fixed;
-            top: 50%;
-            margin-left: auto;
-            margin-right: auto;
-            width: auto;
-            height: auto;
-            box-sizing: content-box;
-            background-color: white;
-            display: none;
-            justify-content: center;
-            align-items: center;
-        }
-        #modal-content
-        {
-            padding: 20px;
-            text-align: center;
-        }
-        #modal-content p{
-            margin: 0 0 20px;
-        }
-        #modal-content button {
-            margin: 0 10px;
-            padding: 5px 10px;
-            cursor: pointer;
-        }
-        #cancelModal{
-            background-color: rgb(237, 242, 237);
-        }
-        #addModal{
-            background-color: rgb(49, 130, 206);
-            color: white;
-        }
-    </style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
     <div class="container">
@@ -140,68 +54,77 @@
             <label class="sz-20" for="add">Alamat Lengkap</label> <br>
             <textarea class="sz-20 wdt100" id="add" name="add" placeholder="Alamat Lengkap" required></textarea>
             <br>
-            <button class="sub" type="submit" id="submit">+ Add Data</button>
-
-            <div id="modal">
-                <div id="modal-content">
-                    <p>Tambah Biodata</p>
-                    <p>Anda Akan Menambahkan Data Biodata Baru</p>
-                    <button id="cancelModal">Cancel</button>
-                    <button id="addModal">Add</button>
+            <button class="sub" type="submit" id="submit" data-toggle="modal" data-target="#myModal">+ Add Data</button>
+        </form>
+    </div>
+    <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+            <div id="modal-content">
+                <div id="modal-header">
+                    <h4 id="modal-title">Tambah Biodata</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div>
+                   <h5 id="modal-subtitle">Anda Akan Menambahkan Data Biodata Baru</h5>
+                 </div>
+                <div id="modal-footer">
+                    <button id="cancelModal" data-dismiss="modal">Cancel</button>
+                    <button type="submit" id="addModal">Add</button>
                 </div>
             </div>
-        </form>
+        </div>        
+    </div>
         <?
-            if($_SERVER["REQUEST_METHOD"] === "POST")
-            // if(isset($_POST['submit']))
+            // if($_SERVER["REQUEST_METHOD"] === "POST")
+            if(isset($_POST['submit']))
             {
-                // if(isset($_POST['name']))
-                // {
+                if(isset($_POST['name']))
+                {
                     $name = $_POST['name'];
-                // }
-                // else{
-                //     $error = "Input Nama Tidak Boleh Kosong";
-                // }
+                }
+                else{
+                    $error = "Input Nama Tidak Boleh Kosong";
+                }
 
-                // if(isset($_POST['sex']))
-                // {
+                if(isset($_POST['sex']))
+                {
                     $sex = $_POST['sex'];
-                // }
-                // else{
-                //     $error = "Pilih Salah Satu Jenis Kelamin";
-                // }
+                }
+                else{
+                    $error = "Pilih Salah Satu Jenis Kelamin";
+                }
 
-                // if(isset($_POST['bp']))
-                // {    
+                if(isset($_POST['bp']))
+                {    
                     $bp = $_POST['bp'];
-                // }
-                // else{
-                //     $error = "Input Tempat Lahir Tidak Boleh Kosong";
-                // }
+                }
+                else{
+                    $error = "Input Tempat Lahir Tidak Boleh Kosong";
+                }
 
-                // if(isset($_POST['bd']))
-                // {
+                if(isset($_POST['bd']))
+                {
                     $bd = $_POST['bd'];
-                // }
-                // else{
-                //     $error = "Input Tanggal Lahir Tidak Boleh Kosong";
-                // }
+                }
+                else{
+                    $error = "Input Tanggal Lahir Tidak Boleh Kosong";
+                }
 
-                // if(isset($_POST['rel']))
-                // {
+                if(isset($_POST['rel']))
+                {
                     $rel = $_POST['rel'];
-                // }
-                // else{
-                //     $error = "Pilih Salah Satu Agama";
-                // }
+                }
+                else{
+                    $error = "Pilih Salah Satu Agama";
+                }
 
-                // if(isset($_POST['add']))
-                // {
+                if(isset($_POST['add']))
+                {
                    $add = $_POST['add'];
-                // }
-                // else{
-                //     $error = "Input Alamat Lengkap Tidak Boleh Kosong";
-                // }
+                }
+                else{
+                    $error = "Input Alamat Lengkap Tidak Boleh Kosong";
+                }
 
                 $_SESSION['name'] = $name;
                 $_SESSION['sex'] = $sex;
@@ -221,22 +144,33 @@
             const cancelModalBtn = document.getElementById('cancelModal');
             const submitBtn = document.getElementById('submit');
 
-            form.addEventListener('submit', function(e) {
-            e.preventDefault();
-            modal.style.display = 'flex';
-            });
+            var name = document.getElementById("name").value;
+            var sex = document.getElementById("sex").value;
+            var bp = document.getElementById("bp").value;
+            var bd = document.getElementById("bd").value;
+            var rel = document.getElementById("rel").value;
+            var add = document.getElementById("add").value;
 
-            submitModalBtn.addEventListener('click', function() {
-            form.submit();
-            });
+            // if (name == '' || sex == '' || bp == '' || bd == '' || rel == '' || add == '') {
+            //     alert('Mohon isi semua field terlebih dahulu.');
+            // } else {
+                // Tampilkan modal jika semua field sudah diisi
+            // form.addEventListener('submit', function(e) {
+            //     e.preventDefault();
+            //     modal.style.display = 'flex';
+            // });
 
-            cancelModalBtn.addEventListener('click', function() {
-            modal.style.display = 'none';
-            });
+            // submitModalBtn.addEventListener('click', function(event) {
+            //     form.submit();
+            // });
 
-            submitBtn.addEventListener('click', function() {
-            form.submit();
-            });
+            // submitBtn.addEventListener('click', function() {
+            //     form.submit();
+            // });
+            
+            // }
+            
+            
         </script>
     </div>
 </body>
